2dc450f2a6c16469d4d3c77e9d7270a41305c60f
Dmitry Sazhin <dmitry.sazhin@acquire.io>
2024-05-16 15:55:21 +0300

    add test scenarios

--- generate_tests.sh
+++ generate_tests.sh
@@ -15,6 +15,15 @@
 # generate append to 1 log 2GB size by 1 writer, batch 500 by 1KB
 ./build/perftests generateCfg auto append 1 2147483648 1 500 1024
 #------------------
+# generate append to 1 log 2GB size by 10 writer, batch 500 by 100KB
+./build/perftests generateCfg auto append 1 2147483648 10 500 102400
+
+# generate append to 1 log 2GB size by 1 writer, batch 500 by 10KB
+./build/perftests generateCfg auto append 1 2147483648 10 500 10240
+
+# generate append to 1 log 2GB size by 1 writer, batch 500 by 1KB
+./build/perftests generateCfg auto append 1 2147483648 10 500 1024
+#------------------
 # generate append to 10 log 200MB size by 1 writer, batch 500 by 100KB
 ./build/perftests generateCfg auto append 10 214748364 1 500 102400
 
